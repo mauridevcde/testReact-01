5a4a2ff6074252bcb6a6a22f6ade6b7d794535b6
Mauricio Gonzalez <102036848+mauridevcde@users.noreply.github.com>
2022-08-20 16:09:53 -0400

    testing..

--- __tests__/CounterApp.test.jsx
+++ __tests__/CounterApp.test.jsx
@@ -1,20 +1,20 @@
 
 import { fireEvent, render, screen } from "@testing-library/react";
-import {CounterApp} from "../src/CounterApp";
+import { CounterApp } from "../src/CounterApp";
 
 // test: debe de hacer match con el snapshot
 //test: debe de mostrar el valor inicial de 100
 
-// describe('CounterApp', () => {
+// describe('CounterApp', () => { 
 
 //     const valor = 20;
 
 //     test('hacer match con el snapshot', () => {
-        
+
 //         const {container} = render(<CounterApp value={valor} />); 
 
 //         expect(container).toMatchSnapshot();
-    
+
 //     })
 
 //     test('mostrar el valor inicial de 100', () => {
@@ -24,7 +24,7 @@ import {CounterApp} from "../src/CounterApp";
 //         expect( screen.getByText('20')).toBeTruthy();
 
 //          expect(screen.getByRole('heading', { level: 2 }).innerHTML).toContain('20')
-    
+
 //     })
 
 // })
@@ -34,14 +34,18 @@ describe('CounterApp captuando eventos', () => {
     const valor = 20;
 
     test('debe incrementar con el boton +1', () => {
-        
-        const {container} = render(<CounterApp value={valor} />); 
+
+        render(<CounterApp value={valor} />);
 
         //primeramente ejecutamos un evento con fireEvent
-        fireEvent.click(screen.getByTestId('btnReset'));
+        fireEvent.click(screen.getByTestId('btnReset')); //capturamos el boton reset
 
-        expect( screen.getByTestId('btnReset') ).toBeTruthy();
-    
+        expect(screen.getByTestId('btnReset')).toBeTruthy(); //verificamos que el boton reset exista
+
+        expect(screen.getByText(valor)).toBeTruthy(); //verificamos que el valor inicial sea 20
+
+
+
     })
 
 
